9cbee9ca659cd2839ec6cb81e2e58130c97be117
23ua <artefacter@gmail.com>
2015-02-15 19:34:14 +0200

    Add link_dotfiles script

---
+++ link_dotfiles.sh
@@ -0,0 +1,23 @@
+home_folder="$HOME"
+
+handle_file () {
+	if [ ! -f $1 ]; then
+		echo "Error: no such dotfile: $1"
+		return
+	fi
+
+	# backup old file
+	if [ -h $home_folder/$1 ]; then
+		rm $home_folder/$1 && echo "$home_folder/$1 is a symlink; rm $1 performed"
+	elif [ -f $home_folder/$1 ]; then
+		mv $home_folder/$1 $home_folder/$1.bkp && echo "$home_folder/$1 already exists; moved to $1.bkp"
+	fi
+
+	ln -s $(pwd)/$1 $home_folder/$1 && echo "$home_folder/$1 was linked"
+}
+
+
+for dotfile in ".zshrc" ".vimrc" ".tmux.conf"; do
+	handle_file $dotfile
+done
+
