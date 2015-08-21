18896b7866537c4e13dc61311b2626ccb782f95a
23ua <artefacter@gmail.com>
2015-08-21 16:50:09 +0300

    Fix echo comment

--- install_all.sh
+++ install_all.sh
@@ -5,5 +5,5 @@ echo "This will install os packages, vim plugins and any other env tools"
 ./install_packages.sh
 ./install_env.sh
 
-echo "All env and pacgages installed"
+echo "All env and packages installed"
 
