46ebc6678bdb5fbd0b7ac312e64071bc4c0c1529
23ua <artefacter@gmail.com>
2016-02-22 16:18:16 +0200

    Fix .ideavimrc name

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -20,7 +20,7 @@ handle_file () {
 }
 
 
-for dotfile in ".zshrc" ".vimrc" ".tmux.conf" ".zshenv" ".ideavim"; do
+for dotfile in ".zshrc" ".vimrc" ".tmux.conf" ".zshenv" ".ideavimrc"; do
 	handle_file $dotfile
 done
 
